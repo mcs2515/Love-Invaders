ae9a1d5961310bb1d3aa6cc6b87f38867e760abb
James Spinelli (RIT Student) <jps8717@ad.rit.edu>
2017-04-15 19:23:18 -0400

    Setup MovableObjects

--- Game/MovableObjects.cpp
+++ Game/MovableObjects.cpp
@@ -1,7 +1,18 @@
 #include "MovableObjects.h"
 
-MovableObjects::MovableObjects()
+void MovableObjects::Move()
 {
+}
+
+MovableObjects::MovableObjects(vector3 ip, vector3 ir, vector3 is)
+{
+	position = ip;
+	rotation = ir;
+	size = is;
+	modelMatrix = IDENTITY_M4;
+	modelMatrix *= position;
+	modelMatrix *= rotation;
+	modelMatrix *= size;
 }
 
 
--- Game/MovableObjects.h
+++ Game/MovableObjects.h
@@ -1,7 +1,15 @@
 #pragma once
+#include "RE\ReEngAppClass.h"
+
 class MovableObjects
 {
 public:
-	MovableObjects();
+	matrix4 modelMatrix;
+	vector3 position;
+	vector3 rotation;
+	vector3 size;
+
+	virtual void Move();
+	MovableObjects(vector3 ip, vector3 ir, vector3 is);
 	~MovableObjects();
 };
