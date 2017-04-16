1acdd22bd0bd0e4474c4a9aedaa9e18aae2245f8
Megan Smith (RIT Student) <mcs2515@ad.rit.edu>
2017-04-16 17:44:19 -0400

    added new methods to GM

--- Game/GameManager.h
+++ Game/GameManager.h
@@ -1,17 +1,22 @@
 #pragma once
+#include "RE\ReEngAppClass.h"
+#include <SFML\Graphics.hpp>
 #include "Player.h"
 #include "UILabels.h"
+#include "Bullet.h"
+#include "Enemy.h"
 
 class GameManager
 {
 public:
 	GameManager();
 
+	void NextRound(void);
 	void ResetRound(void);
 	void ResetGame(void);
 	
 	
-	//bool CheckGoal();
+	bool CheckGoal(void);
 
 	void ChangeAmmoAmount(void);
 
@@ -19,7 +24,11 @@ public:
 
 	void IncrementCurrentScore(int value);
 
+	float Percentage(float valueToMap, float scaleOrginalMin, float scaleOriginalMax, float mappedMin, float mappedMax);
+
 	void DisplayData(MeshManagerSingleton* meshManager);
+
+	void GameOver(void);
 
 	int GetGoal(void);
 	void SetGoal(int value);
@@ -36,20 +45,25 @@ public:
 	int GetTotalScore(void);
 	void SetTotalScore(int value);
 
+	void SetLives(int value);
+	int GetLives(void);
+
 	UILabels* ui;
 	Player* player;
+	Bullet* bullet;
+	Enemy* enemy;
+	
+	SystemSingleton* m_pSystem;
 
 	~GameManager();
-
+	
 private: 
-	//bullets
-	int ammo;
 	//goal
 	int goal;
+
 	//timer
 	int currentTimer;
-	//win/lose
-	bool roundCleared;
+
 	//score
 	int totalScore;
 	int currentScore;
