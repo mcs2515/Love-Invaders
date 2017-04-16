34d4598e0174fc5bb903be83c046d58cfe7dc269
MAIN\sxl9302 <sxl9302@g.rit.edu>
2017-04-15 23:15:51 -0400

    Changed transform / rotation / scale to work with mat4's instead of vec3's

--- Game/MovableObjects.cpp
+++ Game/MovableObjects.cpp
@@ -17,9 +17,9 @@ MovableObjects::MovableObjects(vector3 ip, vector3 ir, vector3 is)
 	rotation = ir;
 	size = is;
 	modelMatrix = IDENTITY_M4;
-	modelMatrix *= position;
-	modelMatrix *= rotation;
-	modelMatrix *= size;
+	modelMatrix *= glm::translate(position);
+	//modelMatrix *= glm::rotate(position);
+	modelMatrix *= glm::scale(size);
 }
 
 
