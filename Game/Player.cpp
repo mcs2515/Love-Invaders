ff300639a9d080126a5ad42f462b249d649e41b3
MAIN\sxl9302 <sxl9302@g.rit.edu>
2017-04-19 23:57:38 -0400

    Tried to fix linker errors, didn't work...

--- Game/Player.cpp
+++ Game/Player.cpp
@@ -1,41 +1,21 @@
 #include "Player.h"
+/*
 // PRIVATE - CONSTRUCTOR
 Player::Player(vector3 ip, bool ir, vector3 is, MeshManagerSingleton* IMeshManager) : MovableObjects(ip, ir, is, IMeshManager)
 {
-	m_iBullets = 5;
-
-	// Creates the body for drawing - Not used yet
-	m_pBody = new PrimitiveClass();
-	m_pBody->GenerateSphere(1.0f, 5, RERED);
 }
 // PRIVATE - DESTRUCTOR
 
+Player::~Player()
+{
+}
 
 // Draws the body - Not used yet, just adds a sphere to the render list
 void Player::Draw()
 {
-	meshManager->AddSphereToRenderList(modelMatrix, RERED, SOLID);
 }
 
 void Player::Move() {
-	// X AXIS movement
-	if (m_bGoLeft)
-		position.x -= static_cast<float>(m_iMove);
-	else
-		position.x += static_cast<float>(m_iMove);
-
-	// Z AXIS movement
-	if (m_bGoUp)
-		position.z -= static_cast<float>(m_iMove);
-	else
-		position.z += static_cast<float>(m_iMove);
-
-	// Updates the model matrix after moving
-	modelMatrix = glm::translate(position);
-	/*if (!rotation)
-	{
-		modelMatrix *= glm::rotate(180.0f, REAXISY);
-	}*/
-	modelMatrix *= glm::scale(size);
 
 }
+*/
--- Game/Player.h
+++ Game/Player.h
@@ -8,10 +8,19 @@ class Player : MovableObjects
 private:
 
 	// Constructor
-	Player(vector3 ip, bool ir, vector3 is, MeshManagerSingleton* IMeshManager);
+	Player(vector3 ip, bool ir, vector3 is, MeshManagerSingleton* IMeshManager) : MovableObjects(ip, ir, is, IMeshManager) {
+		m_iBullets = 5;
+
+		// Creates the body for drawing - Not used yet
+		m_pBody = new PrimitiveClass();
+		m_pBody->GenerateSphere(1.0f, 5, RERED);
+	};
 
 	// Destructor
-	~Player();
+	~Player() {
+		delete m_pBody;
+		ReleaseInstance();
+	};
 
 	bool m_bGoLeft;						// Getter / Setter -> Set in AppClassControls, used to determine which direction across the Z AXIS the player travels
 	bool m_bGoUp;						// Getter / Setter -> Set in AppClassControls, used to determine which direction across the X AXIS the player travels
@@ -48,21 +57,44 @@ public:
 	}
 	
 	// Draws the Object based on m_pBody and inherited class's variables
-	void Draw();
+	  void Draw() {
+		  meshManager->AddSphereToRenderList(modelMatrix, RERED, SOLID);
+	  };
 
 	// Moves the player based on m_fMove
-	void Move();
+	  void Move() {
 
-	// Getter / Setter for m_bGoLeft
+		  // X AXIS movement
+		  if (m_bGoLeft)
+			  position.x -= static_cast<float>(m_iMove);
+		  else
+			  position.x += static_cast<float>(m_iMove);
+
+		  // Z AXIS movement
+		  if (m_bGoUp)
+			  position.z -= static_cast<float>(m_iMove);
+		  else
+			  position.z += static_cast<float>(m_iMove);
+
+		  // Updates the model matrix after moving
+		  modelMatrix = glm::translate(position);
+		  /*if (!rotation)
+		  {
+		  modelMatrix *= glm::rotate(180.0f, REAXISY);
+		  }*/
+		  modelMatrix *= glm::scale(size);
+	  };
+
+	  // Getter / Setter for m_bGoLeft
 	  bool GetLeft() { return m_bGoLeft; };
 	  void SetLeft(bool l) { m_bGoLeft = l; };
-	// Getter / Setter for m_bGoUp
+	  // Getter / Setter for m_bGoUp
 	  bool GetUp() { return m_bGoUp; };
 	  void SetUp(bool u) { m_bGoUp = u; };
-	// Getter / Setter for m_iLives
+	  // Getter / Setter for m_iLives
 	  int GetLives() { return m_iLives; };
 	  void SetLives(int l) { m_iLives = l; };
-	// Getter / Setter for m_iBullets
+	  // Getter / Setter for m_iBullets
 	  int GetBullets() { return m_iBullets; };
 	  void SetBullets(int b) { m_iBullets = b; };
 
